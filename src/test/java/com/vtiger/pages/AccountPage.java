a76954dbcd2942344df569b0b44493a94f6ba374
Priya <priya31.jain@gmail.com>
2023-05-14 08:54:44 +0530

    create constructor in account page

--- src/test/java/com/vtiger/pages/AccountPage.java
+++ src/test/java/com/vtiger/pages/AccountPage.java
@@ -1,5 +1,21 @@
 package com.vtiger.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.vtiger.utilities.CommonActions;
+
 public class AccountPage {
-int a=9;
+	private WebDriver driver;
+	private CommonActions actions;
+	public ExtentTest logger;
+		
+		public AccountPage(WebDriver driver,ExtentTest logger)
+		{
+			this.driver = driver;
+			this.logger=logger;
+			PageFactory.initElements(driver, this);
+			actions = new CommonActions(driver,logger);
+		}
 }
