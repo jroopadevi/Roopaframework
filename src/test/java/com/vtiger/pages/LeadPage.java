cc241bb68f7d6b835cbb63ca052fa9e769c4b076
Priya <priya31.jain@gmail.com>
2023-05-14 09:22:31 +0530

    Add constructor in lead page

--- src/test/java/com/vtiger/pages/LeadPage.java
+++ src/test/java/com/vtiger/pages/LeadPage.java
@@ -1,5 +1,22 @@
 package com.vtiger.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.vtiger.utilities.CommonActions;
+
 public class LeadPage {
 
+	private WebDriver driver;
+	private CommonActions actions;
+	public ExtentTest logger;
+		
+		public LeadPage(WebDriver driver,ExtentTest logger)
+		{
+			this.driver = driver;
+			this.logger=logger;
+			PageFactory.initElements(driver, this);
+			actions = new CommonActions(driver,logger);
+		}
 }
