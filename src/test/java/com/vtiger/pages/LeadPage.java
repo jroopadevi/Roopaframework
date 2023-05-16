502bc512ef6c51047c5e32ed0b936871631370a4
Roopareddy28 <bujjireddyj@gmail.com>
2023-05-16 15:40:01 +0530

    Added createlead method inLeadPage

--- src/test/java/com/vtiger/pages/LeadPage.java
+++ src/test/java/com/vtiger/pages/LeadPage.java
@@ -1,6 +1,8 @@
 package com.vtiger.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.aventstack.extentreports.ExtentTest;
@@ -19,4 +21,25 @@ public class LeadPage {
 			PageFactory.initElements(driver, this);
 			actions = new CommonActions(driver,logger);
 		}
+		@FindBy(linkText="New Lead")
+		WebElement NewLead;
+		
+		@FindBy(name="lastname")
+		WebElement lastname;
+		
+		@FindBy(name="company")
+		WebElement company;
+		
+		@FindBy(name="button")
+		WebElement save;
+		
+		
+		public void CreateLead(String lname, String comp)
+		{
+			actions.clickElement(NewLead, "link new lead clicked");
+			actions.enterValue(lastname, lname, lname+" has been entered into lastname field");
+			actions.enterValue(company, comp, comp+" has been entered into comapny field");
+			actions.clickElement(save, "save button clicked");
+		}
+		
 }
